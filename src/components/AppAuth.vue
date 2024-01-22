673315d370cbe088b3ed7a36d2be67d6db46e986
Chiranjeeb Gogoi <gogoi.chiranjeeb@rediffmail.com>
2024-01-22 21:44:37 +0530

    Alert displayed on registration form submission

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -79,6 +79,11 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div class="text-white text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant">
+            {{ reg_alert_msg  }}
+          </div>
           <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register" :initial-values="userData">
             <!-- Name -->
             <div class="mb-3">
@@ -155,6 +160,7 @@
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="reg_in_submission"
             >
               Submit
             </button>
@@ -191,12 +197,25 @@ export default {
             },
             userData: {
                 country: "USA"
-            }
+            },
+            reg_in_submission: false,
+            reg_show_alert: false,
+            reg_alert_variant: "bg-blue-500",
+            reg_alert_msg: "Please wait! Your account is being created."
         };
     },
     methods: {
         register(values) {
-          console.log(values);
+            this.reg_in_submission = true;
+            this.reg_show_alert = true;
+            this.reg_alert_variant = "bg-blue-500";
+            this.reg_alert_msg = "Please wait! Your account is being created.";
+            setTimeout(() => {
+                this.reg_in_submission = false;
+                this.reg_show_alert = true;
+                this.reg_alert_variant = "bg-green-500";
+                this.reg_alert_msg = "Your account has been created successfully.";
+            }, 3000);
         }
     }
 }
