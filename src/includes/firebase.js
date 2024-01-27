755b49b4dc6d1b36ff76810ce26eb4397a7bf73e
Chiranjeeb Gogoi <gogoi.chiranjeeb@rediffmail.com>
2024-01-27 22:54:32 +0530

    User registered

---
+++ src/includes/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCrEHfmsjuruTMKyVWM2X5YMzUR21zDz-o",
+    authDomain: "music-c6e13.firebaseapp.com",
+    projectId: "music-c6e13",
+    storageBucket: "music-c6e13.appspot.com",
+    messagingSenderId: "516836587877",
+    appId: "1:516836587877:web:42d9325aed918f98e1098c"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users');
+
+export {
+  auth,
+  db,
+  usersCollection
+}
