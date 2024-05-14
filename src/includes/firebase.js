9f15e41c1aa858f12edd8d8419571d52c2f8002e
Chiranjeeb Gogoi <gogoi.chiranjeeb@rediffmail.com>
2024-05-14 22:39:49 +0530

    progress bar added

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCrEHfmsjuruTMKyVWM2X5YMzUR21zDz-o",
@@ -16,10 +17,13 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
 export {
   auth,
-  usersCollection
+  db,
+  usersCollection,
+  storage
 }
