a6cbbebc6629d5442db6eb90edd3dbd5a67fa00c
Chiranjeeb Gogoi <gogoi.chiranjeeb@rediffmail.com>
2024-07-30 23:02:24 +0530

    Song list updated on new upload

--- src/components/AppUpload.vue
+++ src/components/AppUpload.vue
@@ -42,6 +42,7 @@ import { storage, auth, songsCollection } from "@/includes/firebase";
 
 export default {
     name: "AppUpload",
+    props: ['addSong'],
     data() {
         return {
             is_dragover: false,
@@ -96,7 +97,9 @@ export default {
                     };
 
                     song.url = await task.snapshot.ref.getDownloadURL();
-                    await songsCollection.add(song);
+                    const songRef = await songsCollection.add(song);
+                    const songSnapshot = await songRef.get();
+                    this.addSong(songSnapshot);
 
                     this.uploads[uploadIndex].variant = "bg-green-400";
                     this.uploads[uploadIndex].icon = "fas fa-check";
