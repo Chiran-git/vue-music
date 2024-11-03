85f32b4009c5493027122d1f43e6aea5e242e947
Chiranjeeb Gogoi <gogoi.chiranjeeb@rediffmail.com>
2024-11-03 22:54:05 +0530

    Snapshot tests added

---
+++ src/components/__tests__/snapshot.spec.js
@@ -0,0 +1,24 @@
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
+import AppSongItem from "@/components/AppSongItem.vue";
+
+describe("Snapshot AppSongItem.vue", () => {
+    test('renders correctly', () => {
+        const song = {
+            docID: 'abc',
+            modified_name: 'test',
+            display_name: 'test',
+            comment_count: 5,
+        };
+
+        const wrapper = shallowMount(AppSongItem, {
+            propsData: { song },
+            global: {
+                components: {
+                    'router-link': RouterLinkStub
+                }
+            }
+        });
+
+        expect(wrapper.element).toMatchSnapshot();
+    });
+});
